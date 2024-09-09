fd1565f14f82d6a07e5e8270a417be996161191d
Faimo Mamudo <132302370+Faimo-M@users.noreply.github.com>
2024-09-09 18:21:58 +0200

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,28 @@
+let dados=[ {
+    titulo : "Reinildo Mandava",
+    descricao : "Reinildo Isnard Mandava ou somente Reinildo é um futebolista moçambicano que atua como lateral-esquerdo. Atualmente joga no Atlético de Madrid.",
+    link :"https://www.transfermarkt.pt/reinildo-mandava/profil/spieler/240692",
+    tags :  " renildo beira munhava "},
+
+{
+    titulo : "Geny Catamo",
+    descricao :"Geny Cipriano Catamo é um futebolista moçambicano que atua como atacante. Atualmente joga pelo Sporting e pela Seleção Moçambicana.",
+    link :"https://www.transfermarkt.pt/geny-catamo/profil/spieler/701979",
+    tags :"joia africana leao damost "},
+
+{
+    titulo : "Stanley Ratifo",
+    descricao :"Stanley Ratifo é um futebolista profissional que atua como atacante pelo BSG Chemie Leipzig.Nascido na Alemanha, joga na seleção de Moçambique.",
+    link :"https://www.transfermarkt.pt/stanley-ratifo/profil/spieler/277646",
+    tags: "alemao bagre ponta de lanca numero 9 mocambique"
+},
+
+{
+    
+        titulo : "Alcinda Panguane",
+        descricao : "Alcinda Panguane é uma lutadora de box de nacionalidade Moçambicana ja foi campea de varios torneios nacionais e internacionais",
+        link: "https://www.facebook.com.br",
+        tags: "porrada,luta,campea"}
+
+    
+];
