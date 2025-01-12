e07a83a651355eb7f78e35c799c2a9295da89a69
Faimo Manuel <faimom258@gmail.com>
2025-01-12 21:45:24 +0200

    atualizando os dados,agora tem fotos e a pesquisa eh mais certeira

--- dados.js
+++ dados.js
@@ -2,27 +2,50 @@ let dados=[ {
     titulo : "Reinildo Mandava",
     descricao : "Reinildo Isnard Mandava ou somente Reinildo é um futebolista moçambicano que atua como lateral-esquerdo. Atualmente joga no Atlético de Madrid.",
     link :"https://www.transfermarkt.pt/reinildo-mandava/profil/spieler/240692",
-    tags :  " renildo beira munhava "},
+    tags :  " renildo beira munhava rei atletico de madrid jogador all",
+    redeSocial: "https://www.instagram.com/reinildoo17official/",
+    foto: "media/renildo.jpeg" 
+},
 
 {
     titulo : "Geny Catamo",
     descricao :"Geny Cipriano Catamo é um futebolista moçambicano que atua como atacante. Atualmente joga pelo Sporting e pela Seleção Moçambicana.",
     link :"https://www.transfermarkt.pt/geny-catamo/profil/spieler/701979",
-    tags :"joia africana leao damost "},
+    tags :"joia africana leao damost all ",
+    redeSocial:"https://www.instagram.com/genycatamoficial10/ ",
+    foto: "media/geny.webp" 
+},
 
 {
     titulo : "Stanley Ratifo",
     descricao :"Stanley Ratifo é um futebolista profissional que atua como atacante pelo BSG Chemie Leipzig.Nascido na Alemanha, joga na seleção de Moçambique.",
     link :"https://www.transfermarkt.pt/stanley-ratifo/profil/spieler/277646",
-    tags: "alemao bagre ponta de lanca numero 9 mocambique"
+    tags: "alemao bagre ponta de lanca numero 9 mocambique all",
+    redeSocial: "https://www.instagram.com/_ratifo/ ",
+    foto:  "media/stanley.jpg" 
 },
 
 {
     
         titulo : "Alcinda Panguane",
-        descricao : "Alcinda Panguane é uma lutadora de box de nacionalidade Moçambicana ja foi campea de varios torneios nacionais e internacionais",
-        link: "https://www.facebook.com.br",
-        tags: "porrada,luta,campea"}
+        descricao : "Alcinda Helena Panguana é uma pugilista moçambicana, nascida a 27 de fevereiro de 1994. Alcinda Panguana é vencedora da medalha de prata na categoria até 69kg nos Jogos Africanos de Rabat 2019",
+        link: "https://pt.wikipedia.org/wiki/Alcinda_Panguana",
+        tags: "porrada luta campea pugilista all",
+        redeSocial: "https://www.instagram.com/alcindapanguane/",
+        foto:   "media/Alcinda.jpg" 
+    },
+
+
+{
+    
+            titulo : "Elias Pelembe (Dominguez)",
+            descricao : "Elias Gaspar Pelembe (Maputo, 13 de novembro de 1983), mais conhecido por Domingues e também como Dominguez (pronuncia-se 'Dominguês') é um futebolista moçambicano que atua como meio-campista. Actualmente joga na União Desportiva do Songo",
+            link: "https://pt.wikipedia.org/wiki/Domingues",
+            tags: "puto maravilha domiguez capitao moz atleta all",
+            redeSocial: "https://www.instagram.com/dominguez_pelembe7/",
+            foto: "media/Dominguez.webp" 
+
+            }
 
     
 ];
