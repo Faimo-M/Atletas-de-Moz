2b296c97a2b9ea42dd84e58a537dc3313a2d5cac
Faimo Manuel <faimom258@gmail.com>
2025-01-12 23:13:32 +0200

    adicionado o jogador mexer sitoe

--- dados.js
+++ dados.js
@@ -2,7 +2,7 @@ let dados=[ {
     titulo : "Reinildo Mandava",
     descricao : "Reinildo Isnard Mandava ou somente Reinildo é um futebolista moçambicano que atua como lateral-esquerdo. Atualmente joga no Atlético de Madrid.",
     link :"https://www.transfermarkt.pt/reinildo-mandava/profil/spieler/240692",
-    tags :  " renildo beira munhava rei atletico de madrid jogador all",
+    tags :  " renildo beira munhava rei atletico de madrid jogador all atletas",
     redeSocial: "https://www.instagram.com/reinildoo17official/",
     foto: "media/renildo.jpeg" 
 },
@@ -11,7 +11,7 @@ let dados=[ {
     titulo : "Geny Catamo",
     descricao :"Geny Cipriano Catamo é um futebolista moçambicano que atua como atacante. Atualmente joga pelo Sporting e pela Seleção Moçambicana.",
     link :"https://www.transfermarkt.pt/geny-catamo/profil/spieler/701979",
-    tags :"joia africana leao damost all ",
+    tags :"joia africana leao damost all atletas ",
     redeSocial:"https://www.instagram.com/genycatamoficial10/ ",
     foto: "media/geny.webp" 
 },
@@ -20,20 +20,40 @@ let dados=[ {
     titulo : "Stanley Ratifo",
     descricao :"Stanley Ratifo é um futebolista profissional que atua como atacante pelo BSG Chemie Leipzig.Nascido na Alemanha, joga na seleção de Moçambique.",
     link :"https://www.transfermarkt.pt/stanley-ratifo/profil/spieler/277646",
-    tags: "alemao bagre ponta de lanca numero 9 mocambique all",
+    tags: "alemao bagre ponta de lanca numero 9 mocambique all atletas",
     redeSocial: "https://www.instagram.com/_ratifo/ ",
     foto:  "media/stanley.jpg" 
 },
+
+{
+    titulo : "Bruno Langa",
+    descricao :"Bruno Alberto Langa é um futebolista moçambicano que joga como lateral-esquerdo no UD Almería e na seleção moçambicana. Langa começou a sua carreira sénior em Moçambique, com Maxaquene. ",
+    link :"https://www.transfermarkt.pt/bruno-langa/profil/spieler/631472",
+    tags: "Bruno Alberto Langa numero defesa mocambique all atletas espanha udalmeria lateral",
+    redeSocial: "https://www.instagram.com/brunoallanga5/",
+    foto:  "media/bruno.webp" 
+},
+
 
 {
     
         titulo : "Alcinda Panguane",
         descricao : "Alcinda Helena Panguana é uma pugilista moçambicana, nascida a 27 de fevereiro de 1994. Alcinda Panguana é vencedora da medalha de prata na categoria até 69kg nos Jogos Africanos de Rabat 2019",
         link: "https://pt.wikipedia.org/wiki/Alcinda_Panguana",
-        tags: "porrada luta campea pugilista all",
+        tags: "porrada luta campea pugilista all atletas",
         redeSocial: "https://www.instagram.com/alcindapanguane/",
         foto:   "media/Alcinda.jpg" 
-    },
+ },
+
+ {
+    
+    titulo : "Mexer Sitoe",
+    descricao : "Edson André Sitoe, mais conhecido como Mexer, é um futebolista moçambicano que atua como defesa. Formado nas escolas do Grupo Desportivo de Maputo, atualmente joga pelo bandmarspor",
+    link: "https://www.transfermarkt.pt/mexer/profil/spieler/127610",
+    tags: "mexer  franca bordeux futebolista  defesa central all atletas",
+    redeSocial: "https://www.instagram.com/mexersitoe/",
+    foto:   "media/mexerSitoe.webp" 
+},
 
 
 {
@@ -41,7 +61,7 @@ let dados=[ {
             titulo : "Elias Pelembe (Dominguez)",
             descricao : "Elias Gaspar Pelembe (Maputo, 13 de novembro de 1983), mais conhecido por Domingues e também como Dominguez (pronuncia-se 'Dominguês') é um futebolista moçambicano que atua como meio-campista. Actualmente joga na União Desportiva do Songo",
             link: "https://pt.wikipedia.org/wiki/Domingues",
-            tags: "puto maravilha domiguez capitao moz atleta all",
+            tags: "puto maravilha domiguez capitao moz atletas all",
             redeSocial: "https://www.instagram.com/dominguez_pelembe7/",
             foto: "media/Dominguez.webp" 
 
